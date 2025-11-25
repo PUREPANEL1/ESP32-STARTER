be2eaf32369659fecb2e59892eb8467dd590e602
PUREPANEL1 <yuchang3207@gmail.com>
2025-11-24 21:03:46 -0500

    Use the TFT_eSPI library to display images and implement interactive controls.

---
+++ tfttouch.ino
@@ -0,0 +1,126 @@
+#include <SPI.h>
+#include <TFT_eSPI.h>
+#include <XPT2046_Touchscreen.h>
+#include  "cnmrat.c"
+#include  "gprat.c"
+
+TFT_eSPI tft = TFT_eSPI();
+
+// Touchscreen pins
+#define XPT2046_IRQ 36   // T_IRQ
+#define XPT2046_MOSI 32  // T_DIN
+#define XPT2046_MISO 39  // T_OUT
+#define XPT2046_CLK 25   // T_CLK
+#define XPT2046_CS 33    // T_CS
+#define TFT_RST -1
+
+
+SPIClass touchscreenSPI = SPIClass(VSPI);
+XPT2046_Touchscreen touchscreen(XPT2046_CS, XPT2046_IRQ);
+
+#define SCREEN_WIDTH 320
+#define SCREEN_HEIGHT 240
+#define FONT_SIZE 2
+
+// Touchscreen coordinates: (x, y) and pressure (z)
+int x, y, z;
+int SM=0;
+// Print Touchscreen info about X, Y and Pressure (Z) on the Serial Monitor
+void printTouchToSerial(int touchX, int touchY, int touchZ) {
+  Serial.print("X = ");
+  Serial.print(touchX);
+  Serial.print(" | Y = ");
+  Serial.print(touchY);
+  Serial.print(" | Pressure = ");
+  Serial.print(touchZ);
+  Serial.println();
+}
+
+// Print Touchscreen info about X, Y and Pressure (Z) on the TFT Display
+void printTouchToDisplay(int touchX, int touchY, int touchZ) {
+  // Clear TFT screen
+  tft.fillScreen(TFT_WHITE);
+  tft.setTextColor(TFT_BLACK, TFT_WHITE);
+
+  int centerX = SCREEN_WIDTH / 2;
+  int textY = 80;
+ 
+  String tempText = "X = " + String(touchX);
+  tft.drawCentreString(tempText, centerX, textY, FONT_SIZE);
+
+  textY += 20;
+  tempText = "Y = " + String(touchY);
+  tft.drawCentreString(tempText, centerX, textY, FONT_SIZE);
+
+  textY += 20;
+  tempText = "Pressure = " + String(touchZ);
+  tft.drawCentreString(tempText, centerX, textY, FONT_SIZE);
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  // Start the SPI for the touchscreen and init the touchscreen
+  touchscreenSPI.begin(XPT2046_CLK, XPT2046_MISO, XPT2046_MOSI, XPT2046_CS);
+  touchscreen.begin(touchscreenSPI);
+  // Set the Touchscreen rotation in landscape mode
+  // Note: in some displays, the touchscreen might be upside down, so you might need to set the rotation to 3: touchscreen.setRotation(3);
+  touchscreen.setRotation(1);
+
+  // Start the tft display
+  tft.init();
+  // Set the TFT display rotation in landscape mode
+  tft.setRotation(1);
+
+  // Clear the screen before writing to it
+  tft.fillScreen(TFT_WHITE);
+  tft.setTextColor(TFT_BLACK, TFT_WHITE);
+  
+  // Set X and Y coordinates for center of display
+  int centerX = SCREEN_WIDTH / 2;
+  int centerY = SCREEN_HEIGHT / 2;
+
+  tft.drawCentreString("Hello, world!", centerX, 30, FONT_SIZE);
+  tft.drawCentreString("Touch screen to test", centerX, centerY, FONT_SIZE);
+ 
+}
+void printratboss(){
+  tft.pushImage(0, 0, RATBOSS_WIDTH, RATBOSS_HEIGHT, ratboss);
+}
+
+void printgprat(){
+  tft.pushImage(0, 0, GPRAT_WIDTH, GPRAT_HEIGHT, gprat);
+}
+void printsmtime(){
+   tft.setTextColor(TFT_BLACK, TFT_WHITE);
+  String tempText = "SM count:" + String(SM);
+  tft.drawString(tempText, 250, 120);
+}
+
+/*
+// Checks if Touchscreen was touched, and prints X, Y and Pressure (Z) info on the TFT display and Serial Monitor
+  if (touchscreen.tirqTouched() && touchscreen.touched()) {
+    // Get Touchscreen points
+    TS_Point p = touchscreen.getPoint();
+    // Calibrate Touchscreen points with map function to the correct width and height
+    x = map(p.x, 200, 3700, 1, SCREEN_WIDTH);
+    y = map(p.y, 240, 3800, 1, SCREEN_HEIGHT);
+    z = p.z;
+
+    printTouchToSerial(x, y, z);
+    printTouchToDisplay(x, y, z);
+
+    delay(100);
+  }
+*/
+
+void loop() {
+  if( touchscreen.touched()){
+    tft.pushImage(0, 0, RATBOSS_WIDTH, RATBOSS_HEIGHT, ratboss);
+    printsmtime();
+    ++SM;
+  }else{
+    tft.pushImage(0, 0, GPRAT_WIDTH, GPRAT_HEIGHT, gprat);
+    printsmtime();
+  }
+}
